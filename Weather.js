334a16f88b0db5ceaf49d36a14ca35cd974ecfdb
Egor Khokhlov <egor.unknown05@mail.ru>
2023-06-27 00:13:35 +0500

    feature: implements icons and design

--- Weather.js
+++ Weather.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import propTypes from 'prop-types';
 import { StyleSheet, Text, View } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function Weather({temp, condition}) {
 	return (
 		<View style={styles.container}>
-			<Text>{temp}</Text>
+			<View style={styles.halfContainer}>
+				<Ionicons name="rainy" size={96} />
+				<Text style={styles.temp}>{temp}°</Text>
+			</View>
+			<View style={styles.halfContainer}>
+
+			</View>
 		</View>
 	);
 }
@@ -19,7 +26,15 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		justifyContent: 'center',
-		alignItems: 'center'
+		alignItems: 'center',
+	},
+	halfContainer: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	temp: {
+		fontSize: 42,
 	}
 })
 
